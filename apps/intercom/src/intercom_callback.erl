df11a85edb0ce5276fc9ec9eed3d55e3dbd701c8
Mario Georgiev <mario.georgiev@gmail.com>
2014-03-10 07:59:33 +0200

    Return 'application/json' in response headers

--- apps/intercom/src/intercom_callback.erl
+++ apps/intercom/src/intercom_callback.erl
@@ -5,21 +5,23 @@
 -include_lib("elli/include/elli.hrl").
 -behaviour(elli_handler).
 
+-define(JSON_RESP, {<<"Content-type">>, <<"application/json; charset=UTF-8">>}).
+
 handle(Req, _Args) ->
     %% Delegate to our handler function
     handle(Req#req.method, elli_request:path(Req), Req).
 
 handle('GET', [<<"doorlock">>], _Req) ->
   {ok, Status} = doorlock:status(),
-  {ok, [], jiffy:encode({Status})};
+  {ok, [?JSON_RESP], jiffy:encode({Status})};
 
 handle('PUT', [<<"doorlock">>, <<"unlock">>], _Req) ->
   {ok, Status} = doorlock:unlock(),
-  {ok, [], jiffy:encode({Status})};
+  {ok, [?JSON_RESP], jiffy:encode({Status})};
 
 handle('PUT', [<<"doorlock">>, <<"lock">>], _Req) ->
   {ok, Status} = doorlock:lock(),
-  {ok, [], jiffy:encode({Status})};
+  {ok, [?JSON_RESP], jiffy:encode({Status})};
 
 handle('GET',[<<"hello">>, <<"world">>], _Req) ->
     %% Reply with a normal response. 'ok' can be used instead of '200'
