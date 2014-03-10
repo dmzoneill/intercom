3bf5ba81f859de6fad0a5792a160860e2ed4fb05
Mario Georgiev <mario.georgiev@gmail.com>
2014-03-10 22:10:50 +0200

    Move some defines in config env

--- apps/doorlock/src/doorlock.erl
+++ apps/doorlock/src/doorlock.erl
@@ -32,28 +32,26 @@
 
 -define(SERVER, ?MODULE).
 
--define(PIN, 22).
--define(LOCKED, 0).
--define(UNLOCKED, 1).
--define(TIMEOUT, 5000).
--define(MIN_TIMEOUT, 3000). %% TODO: enforce
--define(MAX_TIMEOUT, 15000).
-
 -record(state, {
   status = locked,
-  timeout = ?TIMEOUT,
+  timeout,
   timeref,
   pin_state
 }).
 
 unlock() ->
-  gen_server:call(?MODULE, {unlock, ?TIMEOUT}).
+  {ok, Timeout} = application:get_env(?MODULE, timeout),
+  gen_server:call(?MODULE, {unlock, Timeout}).
 
-unlock(Timeout) when Timeout =< ?MAX_TIMEOUT ->
-  gen_server:call(?MODULE, {unlock, Timeout});
-
-unlock(_) ->
-  {error, "Timeout exceeds ?MAX_TIMEOUT"}.
+unlock(Timeout) ->
+  {ok, Min} = application:get_env(?MODULE, min_timeout),
+  {ok, Max} = application:get_env(?MODULE, max_timeout),
+  case Timeout of
+    T when (T >= Min) and (T =< Max) ->
+      gen_server:call(?MODULE, {unlock, T});
+    _ ->
+      {error, lists:concat(["Timeout is less than ", Min, " or exceeds ", Max])}
+  end.
 
 lock() ->
   gen_server:call(?MODULE, lock).
@@ -65,8 +63,10 @@ start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 init(_Args) ->
-  {ok, _PinState} = gpio:start_link({?PIN, output}),
-  State = handle_gpio(?LOCKED),
+  {ok, Pin} = application:get_env(?MODULE, pin),
+  {ok, _PinState} = gpio:start_link({Pin, output}),
+  {ok, Locked} = application:get_env(?MODULE, locked),
+  State = handle_gpio(Locked),
   {ok, State}.
 
 handle_call({unlock, Timeout}, _From, State) ->
@@ -74,14 +74,16 @@ handle_call({unlock, Timeout}, _From, State) ->
     true  -> erlang:cancel_timer(State#state.timeref);
     false -> ok
   end,
-  NewState = handle_gpio(?UNLOCKED),
+  {ok, Unlocked} = application:get_env(?MODULE, unlocked),
+  NewState = handle_gpio(Unlocked),
   TimeRef = erlang:send_after(Timeout, self(), autolock),
   %% TODO: create token to prevent more than one person setting the door unlocked?
   UpdatedState = NewState#state{timeref = TimeRef},
   {reply, {ok, response(UpdatedState)}, UpdatedState};
 
 handle_call(lock, _From, _State) ->
-  NewState = handle_gpio(?LOCKED),
+  {ok, Locked} = application:get_env(?MODULE, locked),
+  NewState = handle_gpio(Locked),
   {reply, {ok, response(NewState)}, NewState};
 
 handle_call(status, _From, State) ->
@@ -95,14 +97,16 @@ handle_cast(_Msg, State) ->
   {noreply, State}.
 
 handle_info(autolock, _State) ->
-  NewState = handle_gpio(?LOCKED),
+  {ok, Locked} = application:get_env(?MODULE, locked),
+  NewState = handle_gpio(Locked),
   {noreply, NewState};
 
 handle_info(_Info, State) ->
   {noreply, State}.
 
 terminate(_Reason, _State) ->
-  gpio:release(?PIN),
+  {ok, Pin} = application:get_env(?MODULE, pin),
+  gpio:release(Pin),
   ok.
 
 code_change(_OldVsn, State, _Extra) ->
@@ -111,13 +115,15 @@ code_change(_OldVsn, State, _Extra) ->
 %% private
 
 remaining_ms(State) when is_reference(State#state.timeref) ->
+  {ok, Timeout} = application:get_env(?MODULE, timeout),
   case T = erlang:read_timer(State#state.timeref) of
-    false -> ?TIMEOUT;
+    false -> Timeout;
     _     -> T
   end;
 
 remaining_ms(_) ->
-  ?TIMEOUT.
+  {ok, Timeout} = application:get_env(?MODULE, timeout),
+  Timeout.
 
 response(State) ->
   case State#state.status of
@@ -128,8 +134,11 @@ response(State) ->
   end.
 
 handle_gpio(Lock) ->
-  PinState = gpio:write(?PIN, Lock),
+  {ok, Pin} = application:get_env(?MODULE, pin),
+  {ok, Locked} = application:get_env(?MODULE, locked),
+  {ok, Unlocked} = application:get_env(?MODULE, unlocked),
+  PinState = gpio:write(Pin, Lock),
   case Lock of
-    ?LOCKED   -> #state{status = locked, pin_state = PinState};
-    ?UNLOCKED -> #state{status = unlocked, pin_state = PinState}
+    Locked   -> #state{status = locked, pin_state = PinState};
+    Unlocked -> #state{status = unlocked, pin_state = PinState}
   end.
